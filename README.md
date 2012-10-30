3484f25c993d3c4107bf8f68f88bf242ae906ed0
unknown <lior_g@topq.ramdor.co.il>
2012-10-30 16:37:23 +0200

    start tracking the project

---
+++ README.md
@@ -0,0 +1,5 @@
+# First Application
+
+This application was created according to the android documentation.
+
+You can follow the tutorial here http://developer.android.com/training/basics/firstapp/index.html 
---
+++ src/com/liorginsberg/firstapp/MainActivity.java
@@ -0,0 +1,27 @@
+package com.liorginsberg.firstapp;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends Activity {
+	public final static String EXTRA_MESSAGE = "com.ginsberg.myfirstapp.MESSAGE";
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+}
+
